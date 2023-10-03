4fe48ff5ef8ec65b89ed0adb0b172345dd6db61e
Rodolfo Rolon <kelzemrod@gmail.com>
2023-10-03 18:51:08 +0200

    More components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import './App.css'
 import getGifs from './services/getGifs'
 import Gif from './components/Gif'
+import ListOfGifs from './components/ListOfGifs'
 
 
 
@@ -10,24 +11,14 @@ export default function App (){
   const [gifs, setGifs] = useState([])
 
   useEffect(function() {
-    getGifs({keyword: 'programing'}).then(gifs => setGifs(gifs))
+    getGifs({keyword: 'paraguay'}).then(gifs => setGifs(gifs))
   }, [])
 
   return (
     <div className='App'>
-      <h1>GIFFY App</h1>
       <section className='App-content'>
-      {
-        gifs.map(({id, title, url}) => { 
-        <Gif 
-          key={id}
-          id={id}
-          title={title}
-          url={url}
-        />
-      })
-        
-      }   
+      <ListOfGifs gifs={gifs} />
       </section>      
     </div>
-  )}
+  )
+}
